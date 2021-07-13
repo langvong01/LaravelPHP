e1e5c4fd6824ed6fb1b6a23de446a39cc4c81f8c
langvong01 <71130743+langvong01@users.noreply.github.com>
2021-07-13 18:00:59 +0700

    Add files via upload

---
+++ begin/includes/dbh.inc.php
@@ -0,0 +1,7 @@
+<?php
+    $severname = 'localhost';
+    $username = 'root';
+    $password = '';
+    $dbname = 'artstore';
+    $conn = mysqli_connect($severname,$username, $password,$dbname );
+?>
